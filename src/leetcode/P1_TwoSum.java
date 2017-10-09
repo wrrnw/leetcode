7513a1d2ae98a6ea42b547a9550cc6cd38b2286d
Warren Wei <warrenwei1108@gmail.com>
2017-10-09 23:06:24 +1100

    BruteForceVersion

--- src/leetcode/P1_TwoSum.java
+++ src/leetcode/P1_TwoSum.java
@@ -1,5 +1,16 @@
 package leetcode;
 
+
+// Brute force version
 public class P1_TwoSum {
-	
+	public int[] twoSum(int[] nums, int target) {
+		for(int i = 0; i < nums.length; i++) {
+			for(int j = i+1; j < nums.length; j++) {
+				if(target == nums[i] + nums[j]) {
+					return new int[] {i, j};
+				}
+			}
+		}
+		throw new IllegalArgumentException("No two sum solution"); 
+	}
 }
