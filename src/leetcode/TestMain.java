25512ed383eaa281bda9d7fe00914ddeed228301
Warren Wei <warrenwei1108@gmail.com>
2017-10-10 13:51:49 +1100

    TestMain

---
+++ src/leetcode/TestMain.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+public class TestMain {
+	public static void main(String[] args) {
+		P26_RemoveDuplicates a = new P26_RemoveDuplicates();
+		int[] b = {1, 1, 2};
+		System.out.println(a.removeDuplicates(b));
+	}
+}
