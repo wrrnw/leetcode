b9b6ca1c92700284744f11005098f708d9b41451
Warren Wei <warrenwei1108@gmail.com>
2017-10-13 23:53:01 +1100

    updateMain

--- src/array/TestMain.java
+++ src/array/TestMain.java
@@ -2,8 +2,11 @@ package array;
 
 public class TestMain {
 	public static void main(String[] args) {
-		P26_RemoveDuplicates a = new P26_RemoveDuplicates();
-		int[] b = {1, 1, 2};
-		System.out.println(a.removeDuplicates(b));
+		P35_SearchInsert a = new P35_SearchInsert();
+		int[] b = {1, 3, 5, 6};
+		System.out.println(a.searchInsert(b, 0));
+		System.out.println(a.searchInsert(b, 2));
+		System.out.println(a.searchInsert(b, 5));
+		System.out.println(a.searchInsert(b, 7));
 	}
 }
